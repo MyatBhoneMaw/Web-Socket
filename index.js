8699408921f7bd72a1e2f4dfdd63d25acc2dcb4d
Bue <myatbhonemaw72@gmail.com>
2025-05-19 22:05:08 +0630

    message_send_is successfull

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 let express = require('express');
 let socket = require("socket.io");
-/** appsetup  */
+/** appsetup */
 
 let app = express();
 
 /**---------------------set up server */
 let server = app.listen(3000, () => {
-    console.log("This app is running at localhost:3000");
+console.log("This app is running at localhost:3000");
 });
 
 
 /** route setup */
 app.get('/', (res, req) => {
-    req.sendFile(__dirname+ "/public/index.html");
+req.sendFile(__dirname+ "/public/index.html");
 });
 
 /** ---socket.io setup */
 let io = socket(server);
+//Is connection is success?
 io.on('connection', (socket) => {
-    console.log('socket connection is connected');
+//form index.html emit name is chat and callback function parameter is data \
+//data is from chat at index.html
+socket.on("chat", (data) => {
+    io.sockets.emit('chat', data);
+})
+console.log('socket connection is connected');
 })
